c350d9e718e3684610ed33b1bc9991336ad85edc
randradas <randradas@gmail.com>
2019-04-18 21:21:16 +0200

    Rename vim-prepare to setup.sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,7 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
-# Build and deploy VIM stuff
+RANDOM_DIR="/tmp/vim-$RANDOM"
+mkdir -p $RANDOM_DIR
+pushd .
 
-# Build and deploy ~/
-echo "pedo"
+
+# clean and prepare
+rm -rf ~/.vim/autoload ~/.vim/bundle/
+mkdir -p ~/.vim/autoload ~/.vim/bundle/
+
+
+# deploy vim stuff
+cd $RANDOM_DIR
+
+# deploy pathogen
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# deploy vim-colors-solarized
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+
+# deploy vim-puppet
+git clone https://github.com/rodjek/vim-puppet.git
+mv vim-puppet ~/.vim/bundle/
+
+# finish
+rm -rf $RANDOM_DIR
+popd
+
+exit 0
