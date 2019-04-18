e309972d0a646ffb77270f3cc7618ed752ab549c
randradas <randradas@gmail.com>
2019-04-18 21:46:10 +0200

    Fix mv .vimrc file which deletes repo .vimrc file

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
+# flags
 set -e
 
+# temp workspace
 RANDOM_DIR="/tmp/vim-$RANDOM"
 mkdir -p $RANDOM_DIR
 pushd .
+cd $RANDOM_DIR
 
-
-# clean and prepare
+# vim autoload and bundle clean and prepare
 rm -rf ~/.vim/autoload ~/.vim/bundle/
 mkdir -p ~/.vim/autoload ~/.vim/bundle/
 
-
-# deploy vim stuff
-cd $RANDOM_DIR
-
-# deploy pathogen
+# pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# deploy vim-colors-solarized
+# vim-colors-solarized
 git clone git://github.com/altercation/vim-colors-solarized.git
 mv vim-colors-solarized ~/.vim/bundle/
 
-# deploy vim-puppet
+# vim-puppet
 git clone https://github.com/rodjek/vim-puppet.git
 mv vim-puppet ~/.vim/bundle/
 
 # finish
 rm -rf $RANDOM_DIR
 popd
+
+# .vimrc
+cp vim/.vimrc ~
 
 exit 0
