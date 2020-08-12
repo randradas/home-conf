be4511502d60df94cab861496b77769811401903
randradas <randradas@gmail.com>
2020-08-12 18:46:26 +0200

    Simplier VIM install script

--- vim/install.sh
+++ vim/install.sh
@@ -2,25 +2,12 @@
 # flags
 set -e
 
-# temp workspace
-RANDOM_DIR="/tmp/vim-$RANDOM"
-mkdir -p $RANDOM_DIR
-pushd .
-cd $RANDOM_DIR
-
-# vim autoload and bundle clean and prepare
-rm -rf ~/.vim/autoload ~/.vim/bundle/
+# install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle/
-
-# pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# install some plugins
+# install vim-solarized
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colores-solarized
-
-# finish
-rm -rf $RANDOM_DIR
-popd
 
 # .vimrc
 cp vim/.vimrc ~
